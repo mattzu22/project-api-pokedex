24d915c456b6a2c2637d50d5e1e682354a9242a5
Matheus Macedo <matheusg3.201168@gmail.com>
2023-08-21 23:59:12 -0300

    added pokemon skills info

--- src/js/objects/screen-pokemons.js
+++ src/js/objects/screen-pokemons.js
@@ -6,26 +6,27 @@ export const screenPokemons = {
   renderPokemons(poke) {
     let pokemons = poke.map(
       (poke) =>
-      `
-      <div class="cartao-pokemon ${poke.types.join("-")}">
-      <a class="cartao-imagem" href="./pokemon.html?name=${poke.name}">
-      <img src="${poke.image}" id="${poke.name}" alt="${poke.name}">
-      </a>
-      <div class="detalhes">
-      <h2 class="nome">${poke.name}</h2>
-      <div class="tipos">${poke.types
-        .map((type) => {
-          return `<span class="tipo ${type}">${type}</span>`;
-        })
-        .join("")}</div>
-        </div>
-        </div>
         `
-        );
-        this.PokemonsData.innerHTML = "";
-        this.PokemonsData.innerHTML += pokemons;
-        
-        showColorPokemon()
-      },
-    };
-    
+      <div class="cartao-pokemon ${poke.types.join("-")}">
+        <a class="cartao-imagem" href="./pokemon.html?name=${poke.name}">
+        <img src="${poke.image}" id="${poke.name}" alt="${poke.name}">
+        </a>
+
+        <div class="detalhes">
+          <h2 class="nome">${poke.name}</h2>
+          <div class="tipos">${poke.types
+            .map((type) => {
+              return `<span class="tipo ${type}">${type}</span>`;
+            })
+            .join("")}
+            </div>
+        </div>
+      </div>
+        `
+    );
+    this.PokemonsData.innerHTML = "";
+    this.PokemonsData.innerHTML += pokemons;
+
+    showColorPokemon();
+  },
+};
--- src/js/pokemon.js
+++ src/js/pokemon.js
@@ -1,3 +1,6 @@
+
+import showColorPokemon from "./functions/showColorPokemons.js";
+
 const urlParams = window.location.search;
 const params = new URLSearchParams(urlParams);
 const getUrlFlagName = params.get("name");
@@ -7,116 +10,120 @@ let containerDetailsPokemon = document.querySelector(".container-pokemons");
 async function getDetailsPokemon(pokemom) {
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemom}`;
   const response = await fetch(url);
-  return await response.json();
-}
+  const pokemonDetails = await response.json();
 
-async function renderPokemon() {
-  const pokemon = await getDetailsPokemon(getUrlFlagName);
-
-  const infoStats = pokemon.stats.map((stat) => {
+  const infoStats = pokemonDetails.stats.map((stat) => {
     const nameStats = stat.stat.name;
     const baseStats = stat.base_stat;
     return { nameStats, baseStats };
   });
 
-  const { moves } = pokemon;
+  const { moves } = pokemonDetails;
   const movesSelect = moves.slice(0, 4);
 
-  const { abilities } = pokemon;
+  const { abilities } = pokemonDetails;
 
-  const nameAbilities = abilities.map((ability) => ability.ability.name);
+  const types = pokemonDetails.types.map((type) => type.type.name);
 
   const urlAbilityPromises = abilities.map(async (ability) => {
-    const nameAbility = ability.ability.name;
     const urlAbility = ability.ability.url;
     const response = await fetch(urlAbility);
     const json = await response.json();
-    const description = json.effect_entries;
-    const descriptionAbility = description
-      .filter((val) => val.language.name === "en")
-      .map((val) => ({
-        nameAbility: nameAbility,
-        effect: val.effect,
-      }));
-    return descriptionAbility;
+    return {
+      nameAbility: ability.ability.name,
+      effects: json.effect_entries.filter((entry) => entry.language.name === "en"),
+    };
   });
 
-  Promise.all(urlAbilityPromises).then((results) => {
-    results.map((result) => {
-      detailPokemon.push({
-        nameAbility: result[0].nameAbility,
-        effect: result[0].effect,
-      });
-    });
+  Promise.all(urlAbilityPromises).then((abilityResults) => {
+    detailPokemon.push({
+      image: pokemonDetails.sprites.front_default,
+      id: pokemonDetails.id,
+      name: pokemonDetails.name,
+      types: types,
+      movesSelect: movesSelect,
+      stats: infoStats,
+      abilities: abilityResults,
+    })
+
+    renderPokemon(detailPokemon);
   });
+}
 
-  const types = pokemon.types.map((type) => type.type.name);
+getDetailsPokemon(getUrlFlagName);
 
-  detailPokemon.push({
-    image: pokemon.sprites.front_default,
-    id: pokemon.id,
-    name: pokemon.name,
-    types: types,
-    movesSelect: movesSelect,
-    stats: infoStats,
-  });
-
-  detailPokemon.map((detail) => {
+function renderPokemon(detailPokemon) {
+  console.log(detailPokemon);
+  detailPokemon.map((detail, index) => {
     containerDetailsPokemon.innerHTML += `
-    <div class="detail-pokemon">
-    <div class="info-right ${detail.types.join("-")}">
+  <div class="detail-pokemon">
+    <div class="info-right ${detail.types.join("-")} cartao-pokemon">
       <div class="info-pokemon">
         <div class="name-type">
           <h2 class="name">${detail.name}</h2>
           <div class="types">${detail.types
             .map((type) => {
-              return `<span class="type ${type}">${type}</span>`;
+              return `<span class="type tipo ${type}">${type}</span>`;
             })
             .join("")}</div>
           </div>  
         <p class="number">#00${detail.id}</p>
 
         <button class="btn-shiny"></button>
-      </div>
+    </div>
 
-      <div class="container-poke-details">
+    <div class="container-poke-details">
         <img src=${detail.image} class="img-pokemon"/>
             
         <div class="poke-details">
-          <nav>
+          <nav class="navegation">
             <ul>
-              <li>Status</li>
-              <li>Habilidades</li>
-              <li>Moves</li>
+              <li class="selecionado" id=${index}>Status</li>
+              <li id=${index}>Habilidades</li>
+              <li id=${index}>Moves</li>
             </ul>
           </nav>
 
-          <div class="status">
-            <h3>Status</h3>
-
-            <ul>
-              ${detail.stats
-                .map((stat) => {
-                  return `<li>${stat.nameStats}: ${stat.baseStats}</li>`;
-                })
-                .join("")}
-            </ul>
-          </div>
-         
-
-          <div class="abilities">
-              <h3>Habilidades</h3>
+          <div class="info">    
+            <div class="status" id="status-0">
+              <h3>Status</h3>
 
               <ul>
-                ${detail.movesSelect
-                  .map((moves) => {
-                    return `
-                  <li>${moves.move.name}</li>
-                  `;
+                ${detail.stats
+                  .map((stat) => {
+                    return `<li>${stat.nameStats}: ${stat.baseStats}</li>`;
                   })
                   .join("")}
               </ul>
-          </div>
+            </div>
+          
+
+            <div class="moves" id="moves-1">
+                <h3>Moves</h3>
+
+                <ul>
+                  ${detail.movesSelect
+                    .map((moves) => {
+                      return `
+                    <li>${moves.move.name}</li>
+                    `;
+                    })
+                    .join("")}
+                </ul>
+            </div>
+
+            <div class="abilities" id="abilities-2">
+                <h3>Abilities</h3>
+
+                <ul>  
+                  ${detail.abilities.map( result =>{
+                    return `
+                      <li>${result.nameAbility}: ${result.effects[0].effect}</li>
+                    `
+                  }).join("")}.
+                </ul>
+            </div>
+          </div> 
         </div>  
       </div>
     </div>
@@ -124,11 +131,11 @@ async function renderPokemon() {
     `;
   });
 
-  const btnShiny = document.querySelector(".btn-shiny");
+  // const btnShiny = document.querySelector(".btn-shiny");
 
-  btnShiny.addEventListener("click", () => {
-    changerPokemonShiny(btnShiny);
-  });
+  // btnShiny.addEventListener("click", () => {
+  //   changerPokemonShiny(btnShiny);
+  // });
 
   showColorPokemon();
 }
@@ -156,62 +163,3 @@ async function changerPokemonShiny(btn) {
     }, 200);
   }
 }
-
-renderPokemon();
-
-function showColorPokemon() {
-  const cardPokemon = document.querySelectorAll(".info-right");
-  const typesPokemon = document.querySelectorAll(".type");
-
-  const bgCardPokemon = {
-    fire: "tipo-fogo",
-    grass: "tipo-planta",
-    water: "tipo-agua",
-    bug: "tipo-inseto",
-    normal: "tipo-normal",
-    poison: "tipo-venenoso",
-    flying: "tipo-voador",
-    ground: "tipo-terra",
-    electric: "tipo-eletrico",
-    fairy: "tipo-fada",
-    fighting: "tipo-lutador",
-    psychic: "tipo-psiquico",
-    "water-poison": "tipo-agua-veneno",
-    "water-fighting": "tipo-agua-lutador",
-    "normal-fairy": "tipo-normal-fada",
-    "poison-flying": "tipo-veneno-voador",
-    "poison-ground": "tipo-veneno-terra",
-    "grass-poison": "tipo-planta-veneno",
-    "fire-flying": "tipo-voador-fire",
-    "bug-flying": "tipo-inseto-voador",
-    "normal-flying": "tipo-normal-voador",
-    "bug-poison": "tipo-inseto-veneno",
-    "bug-grass": "tipo-inseto-planta",
-  };
-
-  typesPokemon.forEach((element) => {
-    const types = element.classList;
-
-    types.forEach((val) => {
-      if (val in bgCardPokemon) {
-        element.classList.add(bgCardPokemon[val]);
-      }
-    });
-  });
-
-  cardPokemon.forEach((card) => {
-    const types = card.classList;
-
-    types.forEach((type) => {
-      if (type in bgCardPokemon) {
-        card.classList.add(bgCardPokemon[type]);
-      }
-    });
-  });
-}
-
-// ${detail.nameAbilities
-//   .map((ability) => {
-//     return `<p>${ability}</p>`;
-//   })
-//   .join(" ")}
